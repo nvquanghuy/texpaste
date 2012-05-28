fb192ed1847b68338a44176e99e3637b246d2a79
Huy Nguyen <nvquanghuy@gmail.com>
2012-05-28 15:22:04 +0800

    Remove equation labeling

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,7 +1,6 @@
 
 $( function() {
     MathJax.Hub.Config({
-        TeX: { equationNumbers: { autoNumber: "AMS" } },
         extensions: ["tex2jax.js"],
         jax: ["input/TeX","output/HTML-CSS"],
         tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
