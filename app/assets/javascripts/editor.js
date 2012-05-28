2b3f4032fc574e875939155432ac17d7fa088182
Huy Nguyen <nvquanghuy@gmail.com>
2012-05-28 14:45:14 +0800

    Added equation labeling

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,6 +1,7 @@
 
 $( function() {
     MathJax.Hub.Config({
+        TeX: { equationNumbers: { autoNumber: "AMS" } },
         extensions: ["tex2jax.js"],
         jax: ["input/TeX","output/HTML-CSS"],
         tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
