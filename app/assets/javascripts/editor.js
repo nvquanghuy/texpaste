b6d9ea8b31596343b9e64c4b18f070039d213793
Huy Nguyen <huy@viki.com>
2013-09-03 22:48:33 +0800

    Fix insert at caret

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,66 +1,71 @@
+$(function () {
+  MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"]
+    ]}
+  });
 
-$( function() {
-    MathJax.Hub.Config({
-        extensions: ["tex2jax.js"],
-        jax: ["input/TeX","output/HTML-CSS"],
-        tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
-    });
-
-} );
+});
 
 function convertTextToMathJaxReady(a) {
   if (!a) return "";
-  
-    var b = a.split("\n");
-    for (var c = 0; c < b.length; c++) {
-        var d = 0;
-        while (d < b[c].length && b[c][d] === " ") d++;
-        var e = "";
-        for (var f = 0; f < d; f++) e += "&nbsp;";
-        e != "" && (b[c] = e + b[c])
-    }
-    return b.join("<br>");
+
+  var b = a.split("\n");
+  for (var c = 0; c < b.length; c++) {
+    var d = 0;
+    while (d < b[c].length && b[c][d] === " ") d++;
+    var e = "";
+    for (var f = 0; f < d; f++) e += "&nbsp;";
+    e != "" && (b[c] = e + b[c])
+  }
+  return b.join("<br>");
 }
 
-jQuery.fn.extend({
-insertAtCaret: function(myValue){
-  return this.each(function(i) {
-    if (document.selection) {
-      //For browsers like Internet Explorer
-      this.focus();
-      sel = document.selection.createRange();
-      sel.text = myValue;
-      this.focus();
+$(function() {
+  $.fn.extend({
+    insertAtCaret: function (myValue) {
+      return this.each(function (i) {
+        if (document.selection) {
+          //For browsers like Internet Explorer
+          this.focus();
+          sel = document.selection.createRange();
+          sel.text = myValue;
+          this.focus();
+        }
+        else if (this.selectionStart || this.selectionStart == '0') {
+          //For browsers like Firefox and Webkit based
+          var startPos = this.selectionStart;
+          var endPos = this.selectionEnd;
+          var scrollTop = this.scrollTop;
+          this.value = this.value.substring(0, startPos) + myValue + this.value.substring(endPos, this.value.length);
+          this.focus();
+          this.selectionStart = startPos + myValue.length;
+          this.selectionEnd = startPos + myValue.length;
+          this.scrollTop = scrollTop;
+        } else {
+          this.value += myValue;
+          this.focus();
+        }
+      })
     }
-    else if (this.selectionStart || this.selectionStart == '0') {
-      //For browsers like Firefox and Webkit based
-      var startPos = this.selectionStart;
-      var endPos = this.selectionEnd;
-      var scrollTop = this.scrollTop;
-      this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
-      this.focus();
-      this.selectionStart = startPos + myValue.length;
-      this.selectionEnd = startPos + myValue.length;
-      this.scrollTop = scrollTop;
-    } else {
-      this.value += myValue;
-      this.focus();
-    }
-  })
-}
+  });
+
 });
 
-$.fn.selectRange = function(start, end) {
-        return this.each(function() {
-            if (this.setSelectionRange) {
-                this.focus();
-                this.setSelectionRange(start, end);
-            } else if (this.createTextRange) {
-                var range = this.createTextRange();
-                range.collapse(true);
-                range.moveEnd('character', end);
-                range.moveStart('character', start);
-                range.select();
-            }
-        });
-    };
+$.fn.selectRange = function (start, end) {
+  return this.each(function () {
+    if (this.setSelectionRange) {
+      this.focus();
+      this.setSelectionRange(start, end);
+    } else if (this.createTextRange) {
+      var range = this.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end);
+      range.moveStart('character', start);
+      range.select();
+    }
+  });
+};
