b99e1bb0d4d4a40dbc44e353056056951b7b2433
Huy Nguyen <huy@viki.com>
2013-09-07 01:25:06 +0800

    Redirect herokuapp domain to main domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,33 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery
+  protect_from_forgery
 
-	helper_method :current_user
+  before_filter :redirect_herokuapp
 
-	private
-		def current_user
-			@current_user ||= User.find(session[:user_id]) if (session[:user_id] && User.exists?(session[:user_id]))
-			return @current_user
 
-			if (@current_user) then
-				return @current_user
-			end
-			if (session[:user_id] and User.exists(session[:user_id])) then
-				@current_user = User.find(session[:user_id])
-			else
-				@current_user = User.new()
-			end
-		  
-		end
+  helper_method :current_user
+
+  private
+
+  def redirect_herokuapp
+    if request.host == 'texpaste.herokuapp.com'
+      qs = request.query_string.present? ? '?' + request.query_string : ''
+      redirect_to request.protocol + 'www.texpaste.com' + request.path + qs
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if (session[:user_id] && User.exists?(session[:user_id]))
+    return @current_user
+
+    if (@current_user) then
+      return @current_user
+    end
+    if (session[:user_id] and User.exists(session[:user_id])) then
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = User.new()
+    end
+
+  end
 
 end
