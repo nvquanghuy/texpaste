95b8020afc5af67169cc122fc36064d1ba313811
Huy Nguyen <nvquanghuy@gmail.com>
2013-11-16 17:59:51 +0800

    Refactor MathJax configuration

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,12 +1,4 @@
 $(function () {
-  MathJax.Hub.Config({
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/HTML-CSS"],
-    tex2jax: {inlineMath: [
-      ["$", "$"],
-      ["\\(", "\\)"]
-    ]}
-  });
 });
 
 function convertTextToMathJaxReady(a) {
